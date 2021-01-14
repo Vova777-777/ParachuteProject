0e3ad15fb158b516d0244553377ae5fcf34fdfc4
Vova777 <vova.klok.00@mail.ru>
2021-01-14 22:44:19 +0300

    coordinateQuarter

--- src/main/java/ru/vova777/coordinatePlane/CoordinateQuarter.java
+++ src/main/java/ru/vova777/coordinatePlane/CoordinateQuarter.java
@@ -7,9 +7,9 @@ public abstract class CoordinateQuarter {
     public abstract int getFinishSectionY(int y0, int length, int azimuth);
 
     public static CoordinateQuarter getNeedfulCoordinateQuarter(int azimuthTrack){
-        if (azimuthTrack >= 0 && azimuthTrack <= 90) return new FirstCoordinateQuarter();
-        else if (azimuthTrack > 90 && azimuthTrack <= 180) return new SecondCoordinateQuarter();
-        else if (azimuthTrack > 180 && azimuthTrack <= 270) return new ThirdCoordinateQuarter();
+        if (0 <= azimuthTrack && azimuthTrack <= 90) return new FirstCoordinateQuarter();
+        else if (90 < azimuthTrack && azimuthTrack <= 180) return new SecondCoordinateQuarter();
+        else if (180 < azimuthTrack && azimuthTrack <= 270) return new ThirdCoordinateQuarter();
         return new FourthCoordinateQuarter();
     }
 }
